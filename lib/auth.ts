5b88f643a98bab682d519c126573d3661519f6b3
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-02-14 19:03:26 +0530

    feat: half-baked auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+//BetterAuth at present >  NextAuth
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//     providers: [
+//         CredentialsProvider({
+
+//         })
+//     ]
+// })
